6102eaf2b70fd10031eb14420ffc0458ce61ba0f
misterrager8 <chemllei0304@gmail.com>
2023-09-21 17:05:15 -0400

    Alphabetize methods

--- src/listo/main.py
+++ src/listo/main.py
@@ -18,47 +18,47 @@ class Listo(list):  # type: ignore [type-arg]
         else:
             self.extend(list(args))
 
-    def first(self) -> typing.Any:
-        return self[0]
-
-    def last(self) -> typing.Any:
-        return self[-1]
-
-    def min(self) -> typing.Any:
-        return min(self)
-
-    def max(self) -> typing.Any:
-        return max(self)
-
-    def random_choice(self) -> typing.Any:
-        # Randomly returns a value from the listo
-        return random.choice(self)
-
     @functools.cached_property
     def _length(self) -> int:
         # Returns caached length of the listo
         return len(self)
 
+    def first(self) -> typing.Any:
+        return self[0]
+
+    def join(self, seperator: str = " ") -> str:
+        # Replicates str.join()
+        return seperator.join([str(x) for x in self])
+
+    def last(self) -> typing.Any:
+        return self[-1]
+
     def len(self) -> int:
         # Returns the length of the listo
         return self._length
+
+    def max(self) -> typing.Any:
+        return max(self)
 
     def median(self) -> typing.Any:
         # Provides the median value, rounding up
         index = round((self._length - 1) / 2)
         return self[index]
 
-    def join(self, seperator: str = " ") -> str:
-        # Replicates str.join()
-        return seperator.join([str(x) for x in self])
+    def min(self) -> typing.Any:
+        return min(self)
 
-    def shuffle(self) -> list[typing.Any]:
-        # Shuffles and resits the listo
-        return random.sample(self, self._length)
+    def random_choice(self) -> typing.Any:
+        # Randomly returns a value from the listo
+        return random.choice(self)
 
     def reverse(self) -> typing.Any:
         # Overwrites the built-in reverse method so a value is returned
         return self[::-1]
+
+    def shuffle(self) -> list[typing.Any]:
+        # Shuffles and resits the listo
+        return random.sample(self, self._length)
 
     reverse.__doc__ = "Overwrites the built-in reverse method so a value is returned"
 
